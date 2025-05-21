58a58fe37f6c017ac17624f42ae9fcbc9d2dab9f
Aramys <bouachar37@gmail.com>
2025-05-21 01:10:14 +0100

    creer window

--- snakegame.py
+++ snakegame.py
@@ -1,2 +1,14 @@
 import pygame
-import tuple
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+screen.fill((0, 0, 0))
+pygame.display.update()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.fill((0, 0, 0))
+    pygame.display.update()
+        
